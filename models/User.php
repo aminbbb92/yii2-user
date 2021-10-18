dd58ced298ddbcc68bc39e49c5826650d7a110cb
amin.shafiee.d@gmail.com <amin.shafiee.d@gmail.com>
2021-10-18 14:14:47 +0330

    Revert "Enh: delete Email required"
    This reverts commit d014c0dd

--- models/User.php
+++ models/User.php
@@ -236,14 +236,14 @@ class User extends ActiveRecord implements IdentityInterface
 
             // email rules
             'emailTrim'     => ['email', 'trim'],
-            //'emailRequired' => ['email', 'required', 'on' => ['register', 'connect', 'create', 'update']],
+            'emailRequired' => ['email', 'required', 'on' => ['register', 'connect', 'create', 'update']],
             'emailPattern'  => ['email', 'email'],
             'emailLength'   => ['email', 'string', 'max' => 255],
-            /*'emailUnique'   => [
+            'emailUnique'   => [
                 'email',
                 'unique',
                 'message' => \Yii::t('user', 'This email address has already been taken')
-            ],*/
+            ],
 
             // password rules
             'passwordRequired' => ['password', 'required', 'on' => ['register']],
